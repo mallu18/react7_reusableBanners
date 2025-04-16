e1708795b656203c1ee7f8a080cbc553e2fc54d0
mallu18 <mallikarjunghasani18@gmail.com>
2025-04-16 17:32:51 +0000

    Solution

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,16 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = ({bannerDetails}) => {
+  const {headerText, description, className} = bannerDetails
+
+  return (
+    <li className={`banner-card ${className}`}>
+      <h1 className="banner-header">{headerText}</h1>
+      <p className="banner-description">{description}</p>
+      <button className="banner-button">Show More</button>
+    </li>
+  )
+}
+
+export default BannerCardItem
